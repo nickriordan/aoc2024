caeaf5ce0177abb008f6f6920f010d2edcf82309
nickriordan <nick@anamatica.com>
2024-12-02 14:59:26 +0000

    Tidy up and simplification

--- src/main/kotlin/day02.kt
+++ src/main/kotlin/day02.kt
@@ -1,11 +1,10 @@
 fun main() {
     val data = loadFileLinesOfIntegers("day02-data.txt")
 
-    fun isSafe(report: List<Int>) = report.windowed(2).let { pairs ->
-        pairs.all { (a, b) -> b > a && b - a < 4 } || pairs.all { (a, b) -> a > b && a - b < 4 }
-    }
+    fun isSafe(rpt: List<Int>) =
+        rpt.windowed(2).let { it.all { (a, b) -> b - a in 1..3 } || it.all { (a, b) -> a - b in 1..3 } }
 
-    fun part1() = data.count { report -> isSafe(report) }
+    fun part1() = data.count { isSafe(it) }
 
     fun part2() = data.count { rpt -> isSafe(rpt) || rpt.indices.any { i -> isSafe(rpt.take(i) + rpt.drop(i + 1)) } }
 
