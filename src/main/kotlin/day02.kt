e381f73c47f5c5a71a735e65f9bf46651f27dbae
nickriordan <nick@anamatica.com>
2024-12-02 10:43:39 +0000

    Part 2 refactor to one line

--- src/main/kotlin/day02.kt
+++ src/main/kotlin/day02.kt
@@ -1,15 +1,13 @@
 fun main() {
     val data = loadFileLinesOfIntegers("day02-data.txt")
 
-    fun isSafeReport(report: List<Int>) = report.windowed(2).let { pairs ->
+    fun isSafe(report: List<Int>) = report.windowed(2).let { pairs ->
         pairs.all { (a, b) -> b > a && b - a < 4 } || pairs.all { (a, b) -> a > b && a - b < 4 }
     }
 
-    fun part1() = data.count { report -> isSafeReport(report) }
+    fun part1() = data.count { report -> isSafe(report) }
 
-    fun part2() = data.count { report ->
-        isSafeReport(report) || report.indices.any { n -> isSafeReport(report.take(n) + report.drop(n + 1)) }
-    }
+    fun part2() = data.count { rpt -> isSafe(rpt) || rpt.indices.any { i -> isSafe(rpt.take(i) + rpt.drop(i + 1)) } }
 
     println(part1())
     println(part2())
