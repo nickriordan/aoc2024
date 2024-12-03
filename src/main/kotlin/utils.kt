8d4ec95c829ae162c476dffc63326bd6b5140340
nickriordan <nick@anamatica.com>
2024-12-03 10:21:25 +0000

    Day 3 - lots of regex!

--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -2,9 +2,12 @@ import java.io.InputStreamReader
 
 private val lineSplitRegex = "[\\s,]+".toRegex()
 
-fun loadFile(fileName: String): Sequence<String> =
+fun loadFile(fileName: String): String =
+    object {}.javaClass.getResourceAsStream(fileName).let { InputStreamReader(it!!).readText() }
+
+fun loadFileAsLines(fileName: String): Sequence<String> =
     object {}.javaClass.getResourceAsStream(fileName).let { InputStreamReader(it!!).readLines().asSequence() }
 
-fun loadFileSplitLine(fileName: String) = loadFile(fileName).map { line -> line.split(lineSplitRegex) }.asSequence()
+fun loadFileSplitLine(fileName: String) = loadFileAsLines(fileName).map { line -> line.split(lineSplitRegex) }.asSequence()
 
 fun loadFileLinesOfIntegers(fileName: String) = loadFileSplitLine(fileName).map { line -> line.map { it.toInt()} }
