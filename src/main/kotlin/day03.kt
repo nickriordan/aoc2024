e04223522d5a2a2d83ebaad4d50599602feb5c60
nickriordan <nick@anamatica.com>
2024-12-03 11:14:21 +0000

    Day 3 - clean up for readability

--- src/main/kotlin/day03.kt
+++ src/main/kotlin/day03.kt
@@ -1,16 +1,19 @@
 fun main() {
     fun part1(src: String) = "mul\\((\\d+),(\\d+)\\)".toRegex().findAll(src)
-        .sumOf { result -> result.groupValues[1].toInt() * result.groupValues[2].toInt() }
+        .sumOf { r -> r.groupValues[1].toInt() * r.groupValues[2].toInt() }
 
-    fun part2(src: String) = "(mul\\((\\d+),(\\d+)\\))|(don't)|(do)".toRegex().findAll(src).fold(true to 0) { acc, r ->
-        when (r.value) {
-            "do" -> true to acc.second
-            "don't" -> false to acc.second
-            else -> if (acc.first) (true to acc.second + (r.groupValues[2].toInt() * r.groupValues[3].toInt())) else acc
-        }
-    }.second
+    fun part2(src: String) = "(mul\\((\\d+),(\\d+)\\))|(don't)|(do)".toRegex().findAll(src)
+        .fold(Pair(true, 0)) { acc, r ->
+            when {
+                r.value == "do" -> Pair(true, acc.second)
+                r.value == "don't" -> Pair(false, acc.second)
+                acc.first -> Pair(true, acc.second + (r.groupValues[2].toInt() * r.groupValues[3].toInt()))
+                else -> acc
+            }
+        }.second
 
-    val data = loadFile("day03-data.txt")
-    println(part1(data))
-    println(part2(data))
+    loadFile("day03-data.txt").also { data ->
+        println(part1(data))
+        println(part2(data))
+    }
 }
