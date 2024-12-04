9a931cc1aeeb230d56a3e61f9bcb9b05f9a79db3
nickriordan <nick@anamatica.com>
2024-12-04 12:13:05 +0000

    Day 4 solution

---
+++ src/main/kotlin/day04.kt
@@ -0,0 +1,36 @@
+fun main() {
+    class TextGrid(val lines: List<String>) : Grid(lines.first().length, lines.size) {
+        private fun charAt(pt: Point) = if (pt.isValid()) lines[pt.y][pt.x] else null
+        fun wordFrom(seq: Sequence<Point>, len: Int) = seq.take(len).map { charAt(it) }.joinToString("")
+
+        fun wordsFromPoint(pt: Point, len: Int) = listOf(
+            wordFrom(pt.northSequence(), len),
+            wordFrom(pt.northEastSequence(), len),
+            wordFrom(pt.eastSequence(), len),
+            wordFrom(pt.southEastSequence(), len),
+            wordFrom(pt.southSequence(), len),
+            wordFrom(pt.southWestSequence(), len),
+            wordFrom(pt.westSequence(), len),
+            wordFrom(pt.northWestSequence(), len)
+        ).filter { it.length == len }
+
+        fun countWordsMatchingFromPoint(word: String) =
+            allPoints().flatMap { wordsFromPoint(it, word.length) }.count { it == word }
+
+        val xMasWords = listOf("MAS", "SAM")
+        fun isXmasPoint(pt: Point): Boolean {
+            return (xMasWords.contains(pt.northWest()?.southEastSequence()?.let { wordFrom(it, 3) }) &&
+                    xMasWords.contains(pt.southWest()?.northEastSequence()?.let { wordFrom(it, 3) }))
+        }
+
+        fun countXMasPoints() = allPoints().count { isXmasPoint(it) }
+    }
+
+
+    val grid = TextGrid(loadFileAsLines("day04-data.txt").toList())
+    fun part1() = grid.countWordsMatchingFromPoint("XMAS")
+    fun part2() = grid.countXMasPoints()
+
+    println(part1())
+    println(part2())
+}
---
+++ src/main/kotlin/grid.kt
@@ -0,0 +1,40 @@
+abstract class Grid(val xSize: Int, val ySize: Int) {
+
+    inner class Point(val x: Int, val y: Int) {
+        private fun north() = if (y > 0) Point(x, y - 1) else null
+        fun northSequence() = generateSequence({ this }) { it.north() }
+
+        private fun northEast() = if (y > 0 && x < xSize - 1) Point(x + 1, y - 1) else null
+        fun northEastSequence() = generateSequence({ this }) { it.northEast() }
+
+        internal fun east() = if (x < xSize - 1) Point(x + 1, y) else null
+        fun eastSequence() = generateSequence({ this }) { it.east() }
+
+        private fun southEast() = if (x < xSize - 1 && y < ySize - 1) Point(x + 1, y + 1) else null
+        fun southEastSequence() = generateSequence({ this }) { it.southEast() }
+
+        private fun south() = if (y < ySize - 1) Point(x, y + 1) else null
+        fun southSequence() = generateSequence({ this }) { it.south() }
+
+        fun southWest() = if (x > 0 && y < ySize - 1) Point(x - 1, y + 1) else null
+        fun southWestSequence() = generateSequence({ this }) { it.southWest() }
+
+        private fun west() = if (x > 0) Point(x - 1, y) else null
+        fun westSequence() = generateSequence({ this }) { it.west() }
+
+        fun northWest() = if (x > 0 && y > 0) Point(x - 1, y - 1) else null
+        fun northWestSequence() = generateSequence({ this }) { it.northWest() }
+
+        internal fun nextSouthLine() = if (y < ySize - 1) Point(0, y + 1) else null
+
+        fun isValid() = x >= 0 && y >= 0 && x < xSize && y < ySize
+
+        override fun equals(other: Any?) = (other === this) || (other is Point && other.x == x && other.y == y)
+
+        override fun hashCode(): Int = x * 13 + y
+
+        override fun toString() = "pt($x, $y)"
+    }
+
+    fun allPoints(): Sequence<Point> = generateSequence({ Point(0, 0) }) { it.east() ?: it.nextSouthLine() }
+}
