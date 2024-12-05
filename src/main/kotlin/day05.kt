6818c7da7adcd3ca0a44c4d4168663d73db9dd94
nickriordan <nick@anamatica.com>
2024-12-05 16:31:11 +0000

    Day 5 - cleanup of naming

--- src/main/kotlin/day05.kt
+++ src/main/kotlin/day05.kt
@@ -1,14 +1,14 @@
 fun main() {
     data class PageOrder(val first: Int, val second: Int)
     data class PrintList(val pages: List<Int>) {
-        fun isValid(orderRules: Set<PageOrder>) = (0..<pages.indices.last).all { firstIx ->
-            pages.drop(firstIx + 1).all { second -> orderRules.contains(PageOrder(pages[firstIx], second)) }
+        fun isValid(pageOrderRules: Set<PageOrder>) = (0..<pages.indices.last).all { firstIx ->
+            pages.drop(firstIx + 1).all { second -> pageOrderRules.contains(PageOrder(pages[firstIx], second)) }
         }
 
-        fun reOrder(orderRules: Set<PageOrder>) =
+        fun reOrder(pageOrderRules: Set<PageOrder>) =
             PrintList((0..<pages.indices.last).fold(pages) { p, firstIx ->
                 (firstIx + 1..p.indices.last).fold(p) { p, secondIx ->
-                    if (orderRules.contains(PageOrder(p[secondIx], p[firstIx]))) {
+                    if (pageOrderRules.contains(PageOrder(p[secondIx], p[firstIx]))) {
                         p.take(firstIx) + p[secondIx] + p.subList(firstIx, secondIx) + p.drop(secondIx + 1)
                     } else
                         pages
@@ -18,22 +18,22 @@ fun main() {
         fun middlePage() = pages[pages.size / 2]
     }
 
-    fun loadPageRules(inp: Sequence<String>) =
+    fun loadPageOrderRules(inp: Sequence<String>) =
         inp.filter { it.contains('|') }.map { it.split('|').map { it.toInt() } }.map { PageOrder(it[0], it[1]) }.toSet()
 
-    fun loadPrintOrdering(lines: Sequence<String>) =
+    fun loadPrintLists(lines: Sequence<String>) =
         lines.filter { it.contains(',') }.map { it.split(',').map { it.toInt() } }.map { PrintList(it) }.toList()
 
     fun part1(orderRules: Set<PageOrder>, printLists: List<PrintList>) =
         printLists.filter { it.isValid(orderRules) }.sumOf { it.middlePage() }
 
-    fun part2(pageRules: Set<PageOrder>, printList: List<PrintList>) =
-        printList.filter { !it.isValid(pageRules) }.sumOf { it.reOrder(pageRules).middlePage() }
+    fun part2(pageOrderRules: Set<PageOrder>, printLists: List<PrintList>) =
+        printLists.filter { !it.isValid(pageOrderRules) }.sumOf { it.reOrder(pageOrderRules).middlePage() }
 
     val data = loadFileAsLines("day05-data.txt")
-    val pageRules = loadPageRules(data)
-    val printOrdering = loadPrintOrdering(data)
+    val pageOrderRules = loadPageOrderRules(data)
+    val printLists = loadPrintLists(data)
 
-    println(part1(pageRules, printOrdering))
-    println(part2(pageRules, printOrdering))
+    println(part1(pageOrderRules, printLists))
+    println(part2(pageOrderRules, printLists))
 }
