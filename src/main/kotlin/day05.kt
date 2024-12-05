9b59c9a89cf706d56d3ffb4976526ad2801a27fd
nickriordan <nick@anamatica.com>
2024-12-05 13:36:25 +0000

    Day 5 solutions

--- src/main/kotlin/day05.kt
+++ src/main/kotlin/day05.kt
@@ -6,16 +6,34 @@ fun main() {
     fun loadPrintOrdering(lines: Sequence<String>) =
         lines.filter { it.contains(',') }.map { it.split(',').map { it.toInt() } }.toList()
 
-    fun validateOrder(pageRules: List<Pair<Int, Int>>, pageList: List<Int>): Boolean {
-        fun check(first: Int, second: Int) = !pageRules.contains(Pair(second, first))
-
-        return (0..pageList.indices.last - 1).all { firstIx ->
-            (firstIx + 1..pageList.indices.last).all { secondIx -> check(pageList[firstIx], pageList[secondIx]) }
+    fun part1(pageRules: List<Pair<Int, Int>>, printOrdering: List<List<Int>>) = printOrdering.sumOf { print ->
+        fun validateOrder(input: List<Int>) = (0..<input.indices.last).all { firstIx ->
+            (firstIx + 1..input.indices.last).all { secondIx -> pageRules.contains(input[firstIx] to input[secondIx]) }
         }
+
+        if (validateOrder(print)) print[print.size / 2] else 0
     }
 
-    fun part1(pageRules: List<Pair<Int, Int>>, printOrdering: List<List<Int>>) = printOrdering.sumOf { print ->
-        if (validateOrder(pageRules, print)) print[print.size / 2] else 0
+    fun part2(pageRules: List<Pair<Int, Int>>, printOrdering: List<List<Int>>): Int {
+        fun reOrder(input: List<Int>): List<Int> {
+            var text = input
+            (0..<input.indices.last).forEach { firstIx ->
+                (firstIx + 1..input.indices.last).forEach { secondIx ->
+                    if (pageRules.contains(Pair(text[secondIx], text[firstIx]))) {
+                        text = text.take(firstIx) + text[secondIx] + text.subList(
+                            firstIx,
+                            secondIx
+                        ) + text.drop(secondIx + 1)
+                    }
+                }
+            }
+            return text
+        }
+
+        return printOrdering.map { it to reOrder(it) }.filter { it.first != it.second }.sumOf {
+            val reOrdered = reOrder(it.second)
+            reOrdered[reOrdered.size / 2]
+        }
     }
 
     val data = loadFileAsLines("day05-data.txt")
@@ -23,4 +41,5 @@ fun main() {
     val printOrdering = loadPrintOrdering(data)
 
     println(part1(pageRules, printOrdering))
+    println(part2(pageRules, printOrdering))
 }
