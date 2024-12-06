0d4063ba4c3b4f4fa0ecbd8957f2d12d19b9e19d
nickriordan <nick@anamatica.com>
2024-12-06 13:25:00 +0000

    Day 6 - unoptimized solution

--- src/main/kotlin/day06.kt
+++ src/main/kotlin/day06.kt
@@ -1,3 +1,46 @@
 fun main() {
+    class LabGrid(private val data: List<String>) : Grid(data[0].length, data.size) {
 
+        inner class Guard(val location: Point, val direction: Char) {
+            override fun toString() = "$direction $location"
+        }
+
+        val guardInitialPosition =
+            (0..<ySize).flatMap { y -> (0..<xSize).map { x -> Point(x, y).let { it to data[it.y][it.x] } } }
+                .first { it.second in listOf('<', '>', '^', 'v') }.let { Guard(it.first, it.second) }
+
+        fun getPath(at: (Point) -> Char) = generateSequence({ guardInitialPosition }) { guard ->
+            fun nextLocation(direction: Char): Guard? =
+                when (direction) {
+                    '^' -> guard.location.north()?.let { if (at(it) == '#') nextLocation('>') else Guard(it, direction) }
+                    'v' -> guard.location.south()?.let { if (at(it) == '#') nextLocation('<') else Guard(it, direction) }
+                    '>' -> guard.location.east()?.let { if (at(it) == '#') nextLocation('v') else Guard(it, direction) }
+                    '<' -> guard.location.west()?.let { if (at(it) == '#') nextLocation('^') else Guard(it, direction) }
+                    else -> throw Exception()
+                }
+
+            nextLocation(guard.direction)
+        }
+
+        fun part1() = getPath { pt: Point -> data[pt.y][pt.x] }.map { it.location }.toSet().size
+
+        fun part2(): Int {
+            val potentialLocations = (0..<ySize).flatMap { y ->
+                (0..<xSize).mapNotNull { x -> if (data[y][x] == '.') Point(x, y) else null }
+            }
+
+            // TODO: we should look for the first two elements of the path
+            // TODO: only need to look along the initial path for options
+            val size = xSize * ySize
+            return potentialLocations.filter { location ->
+                val path = getPath { pt: Point -> if (pt == location) '#' else data[pt.y][pt.x] }
+                path.take(size).count() == size
+            }.size
+        }
+    }
+
+    val data = loadFileAsLines("day06-data.txt")
+    val grid = LabGrid(data.toList())
+    println(grid.part1())
+    println(grid.part2())
 }
--- src/main/kotlin/grid.kt
+++ src/main/kotlin/grid.kt
@@ -1,25 +1,25 @@
 abstract class Grid(val xSize: Int, val ySize: Int) {
 
     inner class Point(val x: Int, val y: Int) {
-        private fun north() = if (y > 0) Point(x, y - 1) else null
+        fun north() = if (y > 0) Point(x, y - 1) else null
         fun northSequence() = generateSequence({ this }) { it.north() }
 
         private fun northEast() = if (y > 0 && x < xSize - 1) Point(x + 1, y - 1) else null
         fun northEastSequence() = generateSequence({ this }) { it.northEast() }
 
-        internal fun east() = if (x < xSize - 1) Point(x + 1, y) else null
+        fun east() = if (x < xSize - 1) Point(x + 1, y) else null
         fun eastSequence() = generateSequence({ this }) { it.east() }
 
         private fun southEast() = if (x < xSize - 1 && y < ySize - 1) Point(x + 1, y + 1) else null
         fun southEastSequence() = generateSequence({ this }) { it.southEast() }
 
-        private fun south() = if (y < ySize - 1) Point(x, y + 1) else null
+        fun south() = if (y < ySize - 1) Point(x, y + 1) else null
         fun southSequence() = generateSequence({ this }) { it.south() }
 
         fun southWest() = if (x > 0 && y < ySize - 1) Point(x - 1, y + 1) else null
         fun southWestSequence() = generateSequence({ this }) { it.southWest() }
 
-        private fun west() = if (x > 0) Point(x - 1, y) else null
+        fun west() = if (x > 0) Point(x - 1, y) else null
         fun westSequence() = generateSequence({ this }) { it.west() }
 
         fun northWest() = if (x > 0 && y > 0) Point(x - 1, y - 1) else null
