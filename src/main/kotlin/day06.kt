38710b06078760cd02a6dc68ff35402dfa914e45
nickriordan <nick@anamatica.com>
2024-12-06 17:23:12 +0000

    Day 6 - more minor tweaks

--- src/main/kotlin/day06.kt
+++ src/main/kotlin/day06.kt
@@ -4,27 +4,25 @@ fun main() {
     data class GuardPos(val location: Grid.Point, val direction: Char)
 
     class LabGrid(private val data: List<String>) : Grid(data[0].length, data.size) {
+        fun pathSequence(at: (Point) -> Char) = generateSequence(
+            { allPoints().first { at(it) in directions }.let { GuardPos(it, at(it)) } }, { guard ->
+                fun move(direction: Char): GuardPos? = when (direction) {
+                    '^' -> guard.location.north()?.let { if (at(it) == '#') move('>') else GuardPos(it, direction) }
+                    'v' -> guard.location.south()?.let { if (at(it) == '#') move('<') else GuardPos(it, direction) }
+                    '>' -> guard.location.east()?.let { if (at(it) == '#') move('v') else GuardPos(it, direction) }
+                    '<' -> guard.location.west()?.let { if (at(it) == '#') move('^') else GuardPos(it, direction) }
+                    else -> throw Exception()
+                }
+                move(guard.direction)
+            })
 
-        val guardInitial = allPoints().first { data[it.y][it.x] in directions }.let { GuardPos(it, data[it.y][it.x]) }
-
-        fun getPath(at: (Point) -> Char) = generateSequence({ guardInitial }) { guard ->
-            fun move(direction: Char): GuardPos? = when (direction) {
-                '^' -> guard.location.north()?.let { if (at(it) == '#') move('>') else GuardPos(it, direction) }
-                'v' -> guard.location.south()?.let { if (at(it) == '#') move('<') else GuardPos(it, direction) }
-                '>' -> guard.location.east()?.let { if (at(it) == '#') move('v') else GuardPos(it, direction) }
-                '<' -> guard.location.west()?.let { if (at(it) == '#') move('^') else GuardPos(it, direction) }
-                else -> throw Exception()
-            }
-            move(guard.direction)
-        }
-
-        fun originalPath() = getPath { data[it.y][it.x] }.map { it.location }.toSet()
+        fun getPath() = pathSequence { data[it.y][it.x] }.map { it.location }.toSet()
 
         fun isPathLoop(path: Sequence<GuardPos>) = mutableSetOf<GuardPos>().let { seen -> path.any { !seen.add(it) } }
 
-        fun part1() = originalPath().size
+        fun part1() = getPath().size
 
-        fun part2() = originalPath().count { loc -> isPathLoop(getPath { if (it == loc) '#' else data[it.y][it.x] }) }
+        fun part2() = getPath().count { loc -> isPathLoop(pathSequence { if (it == loc) '#' else data[it.y][it.x] }) }
     }
 
     val grid = LabGrid(loadFileAsLines("day06-data.txt").toList())
