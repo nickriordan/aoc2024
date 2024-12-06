4fa826a7ae7dff087e4fb0100a4e5cb5f3601032
nickriordan <nick@anamatica.com>
2024-12-06 17:06:09 +0000

    Day 6 naming cleanup

--- src/main/kotlin/day06.kt
+++ src/main/kotlin/day06.kt
@@ -1,26 +1,26 @@
 fun main() {
     val directions = listOf('<', '>', '^', 'v')
 
-    data class Guard(val location: Grid.Point, val direction: Char)
+    data class GuardPos(val location: Grid.Point, val direction: Char)
 
     class LabGrid(private val data: List<String>) : Grid(data[0].length, data.size) {
 
-        val guardInitial = allPoints().first { data[it.y][it.x] in directions }.let { Guard(it, data[it.y][it.x]) }
+        val guardInitial = allPoints().first { data[it.y][it.x] in directions }.let { GuardPos(it, data[it.y][it.x]) }
 
         fun getPath(at: (Point) -> Char) = generateSequence({ guardInitial }) { guard ->
-            fun move(direction: Char): Guard? = when (direction) {
-                '^' -> guard.location.north()?.let { if (at(it) == '#') move('>') else Guard(it, direction) }
-                'v' -> guard.location.south()?.let { if (at(it) == '#') move('<') else Guard(it, direction) }
-                '>' -> guard.location.east()?.let { if (at(it) == '#') move('v') else Guard(it, direction) }
-                '<' -> guard.location.west()?.let { if (at(it) == '#') move('^') else Guard(it, direction) }
+            fun move(direction: Char): GuardPos? = when (direction) {
+                '^' -> guard.location.north()?.let { if (at(it) == '#') move('>') else GuardPos(it, direction) }
+                'v' -> guard.location.south()?.let { if (at(it) == '#') move('<') else GuardPos(it, direction) }
+                '>' -> guard.location.east()?.let { if (at(it) == '#') move('v') else GuardPos(it, direction) }
+                '<' -> guard.location.west()?.let { if (at(it) == '#') move('^') else GuardPos(it, direction) }
                 else -> throw Exception()
             }
             move(guard.direction)
         }
 
-        fun originalPath() = getPath { pt: Point -> data[pt.y][pt.x] }.map { it.location }.toSet()
+        fun originalPath() = getPath { data[it.y][it.x] }.map { it.location }.toSet()
 
-        fun isPathLoop(path: Sequence<Guard>) = mutableSetOf<Guard>().let { seen -> path.any { !seen.add(it) } }
+        fun isPathLoop(path: Sequence<GuardPos>) = mutableSetOf<GuardPos>().let { seen -> path.any { !seen.add(it) } }
 
         fun part1() = originalPath().size
 
