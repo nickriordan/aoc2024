25c1c9aeab9084a5557c0e2612232ddeb878446a
nickriordan <nick@anamatica.com>
2024-12-08 18:59:57 +0000

    Day 7 improve readability

--- src/main/kotlin/day07.kt
+++ src/main/kotlin/day07.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun findTarget(target: Long, src: List<Long>, allowConcat: Boolean): Boolean {
-        fun calc(total: Long, remain: List<Long>): Boolean = if (remain.isEmpty())
-            (total == target)
-        else {
-            calc(total + remain.first(), remain.drop(1)) || calc(total * remain.first(), remain.drop(1)) ||
-                    (allowConcat && calc((total.toString() + remain.first()).toLong(), remain.drop(1)))
-        }
+    fun findTarget(target: Long, src: List<Long>, includeConcat: Boolean): Boolean {
+        fun calc(total: Long, remain: List<Long>): Boolean =
+            if (remain.isEmpty())
+                (total == target)
+            else {
+                calc(total + remain.first(), remain.drop(1)) ||
+                        calc(total * remain.first(), remain.drop(1)) ||
+                        (includeConcat && calc((total.toString() + remain.first()).toLong(), remain.drop(1)))
+            }
+
         return calc(0, src)
     }
 
     val data = loadFileAsLines("day07-test-data.txt")
         .map { it.split(": ") }
-        .map { it[0].toLong() to it[1].split(" ").map { s -> s.toLong() } }.toList()
+        .map { Pair(it[0].toLong(), it[1].split(" ").map { s -> s.toLong() }) }
+        .toList()
 
     fun calculateSum(allowConcat: Boolean) =
         data.filter { equation -> findTarget(equation.first, equation.second, allowConcat) }.sumOf { it.first }
